05a2bb42c19753568f4e41f83f6e89df635b6652
grokify <johncwang@gmail.com>
2024-03-10 10:35:24 -0700

    initial commit

---
+++ cmd/connectors_functionality/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	gosaviyntconnectors "github.com/grokify/go-saviynt-connectors"
+	"github.com/grokify/mogo/fmt/fmtutil"
+)
+
+func main() {
+	dir := "/Users/johnwang/go/src/github.com/saviynt/saviynt-connectors/src/connectors"
+
+	set := gosaviyntconnectors.NewConnectionSet()
+	err := set.ReadDir(dir, true)
+	fmtutil.PrintJSON(err)
+
+	for connKey, conn := range set.Map {
+		fmt.Printf("PROC (%s)\n", connKey)
+		fmtutil.PrintJSON(conn)
+		names := conn.ExternalAttrs.Names(true, true, true, true)
+		fmtutil.PrintJSON(names)
+		break
+	}
+
+	err = set.ExtendedAttributesWriteXLSX("community-connectors.xlsx")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("DONE")
+}
